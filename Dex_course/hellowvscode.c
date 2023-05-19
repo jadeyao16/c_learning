febd38d993088d0377e80fc7a8335062407f81fb
YCL <robert11280809@gmail.com>
2023-05-19 16:57:13 +0800

    Dex course - C Programming tutorial 01~04

---
+++ Dex_course/hellowvscode.c
@@ -0,0 +1,13 @@
+#include <stdio.h> // stdio.h = Standard Library(標準函式庫) 
+
+// Line Comment 
+/* Multiline Comment */
+
+/* Main Function 主函數
+C程式的Entry Point入口點 (執行第一條指令的地方) 
+void=空白 */
+void main(void) {
+    printf("Hello World 1");
+    // printf = Print Formatted 
+    printf("Hello World 2");
+}
